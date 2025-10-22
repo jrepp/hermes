4c9da95b8b364a89cdf2e7b68a5b5af17887be0d
Jacob Repp <jacobrepp@gmail.com>
2025-10-21 17:45:54 -0700

    fix: set loose mode for babel class properties plugins

--- web/babel.config.js
+++ web/babel.config.js
@@ -3,11 +3,13 @@ module.exports = {
     '@babel/preset-env'
   ],
   plugins: [
+    // Decorators must come first (required by Ember)
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     // Add support for JavaScript private methods used by Ember Data 5.7.0
-    '@babel/plugin-proposal-private-methods',
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
     // Support for private fields as well
-    '@babel/plugin-proposal-private-property-in-object',
-    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
     // Transform ember-concurrency async tasks to support arrow functions
     ['ember-concurrency/lib/babel-plugin-transform-ember-concurrency-async-tasks', {
       // Enables async arrow function support for ember-concurrency tasks
